45057b12208ec36075776359d5f7e1eb84a76ea1
Rosalio <149852065+rosaliomayoI@users.noreply.github.com>
2024-04-13 23:01:21 -0600

    Update promotetodc.ps1

--- promotetodc.ps1
+++ promotetodc.ps1
@@ -3,18 +3,22 @@ Import-Module ADDSDeployment
 
 # Define domain administrator credentials
 $domainAdminUsername = "Administrator"
-$domainAdminPassword = ConvertTo-SecureString "YourPassword" -AsPlainText -Force
+$domainAdminPassword = ConvertTo-SecureString "Rmi9306021998@" -AsPlainText -Force
 $domainAdminCredential = New-Object System.Management.Automation.PSCredential ($domainAdminUsername, $domainAdminPassword)
+
+# Define Safe Mode Administrator Password
+$safeModeAdminPassword = ConvertTo-SecureString "Rmi9306021998@" -AsPlainText -Force
 
 # Install AD DS Forest
 Install-ADDSForest -DomainName "mydomain.com" `
                    -DomainNetBiosName "MYDOMAIN" `
                    -ForestMode "WinThreshold" `
                    -DomainMode "WinThreshold" `
-                   -SafeModeAdministratorPassword $domainAdminCredential `
+                   -SafeModeAdministratorPassword $safeModeAdminPassword `
                    -DatabasePath "C:\Windows\NTDS" `
                    -LogPath "C:\Windows\NTDS" `
                    -SysvolPath "C:\Windows\SYSVOL" `
                    -NoRebootOnCompletion:$true
+
 
 
