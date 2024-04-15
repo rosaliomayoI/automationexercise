67a7b48296e8009748fa020d972b4b9d1a2065da
Rosalio <149852065+rosaliomayoI@users.noreply.github.com>
2024-04-15 14:36:25 -0600

    Update installdjango.sh

--- installdjango.sh
+++ installdjango.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# Install Python and pip
+# Update and upgrade the system
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get upgrade -y
 
-# Install Django and django-auth-ldap
-pip3 install django django-auth-ldap
+# Install Python and pip
+sudo apt-get install -y python3-pip python3-dev libpq-dev
 
-# Set up a new Django project
-django-admin startproject myproject
-cd myproject
+# Install Django
+sudo pip3 install django
 
-# Generate settings segment for LDAP (this is a simplified example)
-cat <<EOF >> myproject/settings.py
+# Create a new Django project
+django-admin startproject mydjango
 
-# LDAP settings
-import ldap
-from django_auth_ldap.config import LDAPSearch
+# Change into the project directory and run migrations
+cd mydjango
+python3 manage.py migrate
 
-AUTH_LDAP_SERVER_URI = 'ldap://your_domain_controller_ip'
-
-AUTH_LDAP_BIND_DN = 'cn=read-only-admin,dc=example,dc=com'
-AUTH_LDAP_BIND_PASSWORD = 'yourpassword'
-AUTH_LDAP_USER_SEARCH = LDAPSearch('ou=users,dc=example,dc=com',
-                                   ldap.SCOPE_SUBTREE, '(uid=%(user)s)')
-EOF
+# Start the Django development server on all interfaces
+nohup python3 manage.py runserver 0.0.0.0:8000 &
