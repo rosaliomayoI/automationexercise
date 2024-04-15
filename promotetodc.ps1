1ceaa0e8a39984e1b463c775688cfa951c62519c
Rosalio <149852065+rosaliomayoI@users.noreply.github.com>
2024-04-14 22:00:59 -0600

    Update promotetodc.ps1

--- promotetodc.ps1
+++ promotetodc.ps1
@@ -2,22 +2,33 @@
 Import-Module ADDSDeployment
 
 # Define domain administrator credentials
-$domainAdminUsername = "fox"
+$domainAdminUsername = "Administrator"
 $domainAdminPassword = ConvertTo-SecureString "Rmi9306021998@" -AsPlainText -Force
 $domainAdminCredential = New-Object System.Management.Automation.PSCredential ($domainAdminUsername, $domainAdminPassword)
 
 # Define Safe Mode Administrator Password
 $safeModeAdminPassword = ConvertTo-SecureString "Rmi9306021998@" -AsPlainText -Force
 
-# Install AD DS Forest
-Install-ADDSForest -DomainName "fox.com" `
-                   -DomainNetBiosName "foxdomain" `
-                   -ForestMode "WinThreshold" `
-                   -DomainMode "WinThreshold" `
-                   -SafeModeAdministratorPassword $safeModeAdminPassword `
-                   -DatabasePath "C:\Windows\NTDS" `
-                   -LogPath "C:\Windows\NTDS" `
-                   -SysvolPath "C:\Windows\SYSVOL" `
+try {
+    # Install AD DS Forest
+    Install-ADDSForest -DomainName "fox.com" `
+                       -DomainNetBiosName "foxdomain" `
+                       -ForestMode "WinThreshold" `
+                       -DomainMode "WinThreshold" `
+                       -SafeModeAdministratorPassword $safeModeAdminPassword `
+                       -DatabasePath "C:\Windows\NTDS" `
+                       -LogPath "C:\Windows\NTDS" `
+                       -SysvolPath "C:\Windows\SYSVOL" `
+                       -NoRebootOnCompletion:$true -Force
+
+    Write-Host "AD DS Forest installed successfully."
+    # Additional configuration steps can be added here
+    # Restart the computer to complete installation
+    Restart-Computer
+} catch {
+    Write-Error "Error installing AD DS Forest: $_"
+}
+
                    -NoRebootOnCompletion:$true
 
 
