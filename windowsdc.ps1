fa590e599dab9a5a086194d6b05d8047f384dc88
Rosalio <149852065+rosaliomayoI@users.noreply.github.com>
2024-04-15 16:37:18 -0600

    Update windowsdc.ps1

--- windowsdc.ps1
+++ windowsdc.ps1
@@ -1,5 +1,5 @@
+# Windows domain VM and promotion to domain controller
 
-#Windows domain VM
 # Variables
 $resourceGroupName = "rosalio-onboarding"
 $location = "East US"
@@ -8,13 +8,22 @@ $vnetName = "vnet01"
 $subnetName = "subnet01"
 $publicIpName = "$vmName-PublicIP"
 $nicName = "$vmName-NIC"
+$domainName = "rosalio-local.com"
+$keyVaultName = "rosalio-keyvault"
+
+# Retrieve Secure Credentials from Azure Key Vault
+$secretName = "adminpassword"
+$kvSecret = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $secretName
+$securePassword = ConvertTo-SecureString -String $kvSecret.SecretValueText -AsPlainText -Force
+
+# Network and IP Configuration
 $vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName
 $subnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
 $pip = New-AzPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Static
 $nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id -PublicIpAddressId $pip.Id
 
 # VM Config
-$cred = Get-Credential -Message "Enter a username and password for the VM."
+$cred = New-Object System.Management.Automation.PSCredential ("Administrator", $securePassword)
 $vmConfig = New-AzVMConfig -VMName $vmName -VMSize "Standard_DS1_v2"
 $vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
 $vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2019-Datacenter" -Version "latest"
@@ -22,8 +31,12 @@ $vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
 $osDiskName = "$vmName-OSDisk"
 $vmConfig = Set-AzVMOSDisk -VM $vmConfig -Name $osDiskName -CreateOption FromImage -StorageAccountType "Standard_LRS"
 
-# Output the VM configuration for debugging
-Write-Output $vmConfig
-
 # Create the VM
 New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+
+# Install AD DS Role and Promote to Domain Controller
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $securePassword -Force -NoRebootOnCompletion -Confirm:$false -CreateDnsDelegation:$false
+
+
