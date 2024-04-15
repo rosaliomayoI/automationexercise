0e36a7c95e925abb1e124d83af369fe5b1fc6aab
Rosalio <149852065+rosaliomayoI@users.noreply.github.com>
2024-04-15 16:12:25 -0600

    Update and rename dcvm.tf to windowsdc.ps1

---
+++ windowsdc.ps1
@@ -0,0 +1,26 @@
+
+#Windows domain VM
+# Variables
+$resourceGroupName = "rosalio-onboarding"
+$location = "East US"
+$vmName = "WinDC"
+$vnetName = "vnet01"
+$subnetName = "subnet01"
+$publicIpName = "$vmName-PublicIP"
+$nicName = "$vmName-NIC"
+$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
+$pip = New-AzPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Dynamic
+$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id -PublicIpAddressId $pip.Id
+
+# VM Config
+$cred = Get-Credential -Message "Enter a username and password for the VM."
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize "Standard_DS1_v2"
+$vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2019-Datacenter" -Version "latest"
+$vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
+$osDiskName = "$vmName-OSDisk"
+$vmConfig = Set-AzVMOSDisk -VM $vmConfig -Name $osDiskName -CreateOption FromImage -StorageAccountType "Standard_LRS"
+
+# Create the VM
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
